c9fb14e948ede7724514fbc86859a20a2164844a
M1030797 <M1030797@b2ml29276.mindtree.com>
2018-09-18 16:08:50 +0530

    Array Game

--- src/org/home/array/ArrayGame.java
+++ src/org/home/array/ArrayGame.java
@@ -3,35 +3,33 @@ package org.home.array;
 import java.util.*;
 
 public class ArrayGame {
-	
-	private static boolean isSolvable(int m, int[] arr, int i) {
-	    if (i < 0 || arr[i] == 1) return false;
-	    if ((i == arr.length - 1) || i + m > arr.length - 1) return true;
 
-	    arr[i] = 1;
-	    return isSolvable(m, arr, i + 1) || isSolvable(m, arr, i - 1) || isSolvable(m, arr, i + m);
+	public static boolean canWin(int leap, int[] game, int i) {
+		if (i < 0 || game[i] == 1) {
+			return false;
+		}
+		if ((i == game.length - 1) || i + leap > game.length - 1) {
+			return true;
+		}
+
+		game[i] = 1;
+		return canWin(leap, game, i + 1) || canWin(leap, game, i - 1) || canWin(leap, game, i + leap);
 	}
-    public static boolean canWin(int leap, int[] game) {
-        // Return true if you can win the game; otherwise, return false.
-    	boolean res = false;
-    	int n = game.length;
-    	return(isSolvable(game.length, game, leap));
-    		
-    }
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        int q = scan.nextInt();
-        while (q-- > 0) {
-            int n = scan.nextInt();
-            int leap = scan.nextInt();
-            
-            int[] game = new int[n];
-            for (int i = 0; i < n; i++) {
-                game[i] = scan.nextInt();
-            }
 
-            System.out.println( (canWin(leap, game)) ? "YES" : "NO" );
-        }
-        scan.close();
-    }
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int q = scan.nextInt();
+		while (q-- > 0) {
+			int n = scan.nextInt();
+			int leap = scan.nextInt();
+
+			int[] game = new int[n];
+			for (int i = 0; i < n; i++) {
+				game[i] = scan.nextInt();
+			}
+
+			System.out.println((canWin(leap, game, 0)) ? "YES" : "NO");
+		}
+		scan.close();
+	}
 }
